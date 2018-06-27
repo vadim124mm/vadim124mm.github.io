10128e56f984610177c638058eaf1dd6ca9bd754
vadim124mm <39499578+vadim124mm@users.noreply.github.com>
2018-06-27 21:23:41 +0300

    added

--- FormSender.js
+++ FormSender.js
@@ -1,6 +1,8 @@
 var form = document.forms.user;
 var personName = form.elements["name"];
 var personPhone = form.elements["phone"];
+var bar =  document.getElementById("loading");
+var button = document.getElementById("form-button");
 
 form.submit.addEventListener("click", sendRequest);
 
@@ -8,14 +10,25 @@ function sendRequest(event){
      
     event.preventDefault();
 
-var request = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 function reqReadyStateChange() {
-    if (request.readyState == 4 && request.status == 200)
-        document.getElementById("output").innerHTML=request.responseText;
+    if (xhr.readyState == 4 && xhr.status == 200)
+        document.getElementById("output").innerHTML=xhr.responseText;
 }
-var body = "Name=" + personName.value + "&Phone="+ personPhone.value;
-request.open("POST", "https://cors-anywhere.herokuapp.com/http://telegramsender.somee.com/api/values");
-request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-request.onreadystatechange = reqReadyStateChange;
-request.send(body);
+var body = "Name=" + personName.value + "&Phone="+ personPhone.value + "&ChatID=232076268";
+xhr.open("POST", "http://telegramsender.somee.com/api/values");
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.onreadystatechange = reqReadyStateChange;
+xhr.onloadstart = function () {
+  button.style="display:none";
+  bar.style="display:block"
+  };
+  xhr.onloadend = function () {
+  bar.style="display:none"
+  button.style="display:inline; background-color:#33ff33";
+  button.value="Отправлено!"
+  };
+xhr.send(body);
+
 }
+
