c328853ba25316af48cd4b9e84205a5505864c09
Shliahtur <shliahtur@me.com>
2018-08-13 10:54:34 +0300

    main-change

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.slick_slider').slick({
         slidesToShow: 2,
         slidesToScroll: 2,
-        autoplay: true,
+      
         autoplaySpeed: 2000,
         dots: true,
         responsive: [
